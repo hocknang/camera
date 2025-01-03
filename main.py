f0127317f1c07f9091f0738580b22fa6a56d80cf
peiyuan <fang_peiyuan@yahoo.com>
2025-01-04 03:02:25 +0800

    new

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import streamlit as st
-from pyzbar.pyzbar import decode
+import zxing
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
