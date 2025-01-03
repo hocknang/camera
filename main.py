c897d8f4e61a564a54c8080fcc8e4277a4d46da6
peiyuan <fang_peiyuan@yahoo.com>
2025-01-04 03:49:33 +0800

    new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import cv2
+import cv2 from streamlit_webrtc
 import numpy as np
 import streamlit as st
 
