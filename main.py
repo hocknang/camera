1e8be5dd3656adff94bac2b72f828939d0612510
peiyuan <fang_peiyuan@yahoo.com>
2025-01-04 03:51:21 +0800

    new

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import cv2 from streamlit_webrtc
-import numpy as np
+import cv2
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 import streamlit as st
 
 
@@ -15,23 +15,27 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     st.title("Real-Time Barcode Scanner")
-    cap = cv2.VideoCapture(0)
+    faceCascade = cv2.CascadeClassifier(cv2.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Set camera resolution to a higher value
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)  # Width = 1280 pixels
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # Height = 720 pixels
 
-    # Capture a frame from the camera
-    ret, frame = cap.read()
+    class VideoTransformer(VideoTransformerBase):
+        def __init__(self):
+            self.i = 0
 
-    if not ret:
-        st.write("Error: No camera detected or accessible.")
-    else:
-        # Process the frame for barcode detection
-        frame, barcode_info = detect_barcode(frame)
+        def transform(self, frame):
+            img = frame.to_ndarray(format="bgr24")
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+            i = self.i + 1
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (95, 207, 30), 3)
+                cv2.rectangle(img, (x, y - 40), (x + w, y), (95, 207, 30), -1)
+                cv2.putText(img, 'F-' + str(i), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
 
-        # Display the captured frame in Streamlit
-        st.image(frame, caption="Captured Frame", channels="BGR", use_column_width=True)
+            return img
+
+
+    webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
