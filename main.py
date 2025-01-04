45e679a083e31c784125d38ecc565376568a67c2
peiyuan <fang_peiyuan@yahoo.com>
2025-01-05 00:06:51 +0800

    new

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ if __name__ == '__main__':
                     });
                     
                     startButton.addEventListener("click", () => {
+                        alert("hello world")
                     });
                     
                 });
